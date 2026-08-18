a995e9a20157800311daab51e596df4ef0fdd150
Angel <u23271997@utp.edu.pe>
2026-08-18 09:49:36 -0500

    Primer commit: estructura inicial del proyecto SaaS RRHH

---
+++ backend/rrhh-backend/src/main/java/com/saas/rrhh_backend/RrhhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saas.rrhh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhBackendApplication.class, args);
+	}
+
+}
---
+++ backend/rrhh-backend/src/test/java/com/saas/rrhh_backend/RrhhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.rrhh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrhhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
